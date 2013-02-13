beeb39da809f5e6ad1573744f9c48da7658655e0
Robert Barbey <robert.barbey@gmail.com>
2013-02-13 22:13:45 +0100

    - Added function that jumps to next failure message - Added highlighting for Wrapper starts and stops

--- acrolog-mode.el
+++ acrolog-mode.el
@@ -4,14 +4,28 @@
 ;; define keymap, if few entries, change to 'make-sparse-keymap'
 (defvar acrolog-mode-map
   (let ((map (make-keymap)))
-    (define-key map "\C-r" 'next-check-request)
+    (define-key map "\C-j" 'acrolog-next-failure)
     map)
   "Keymap for Acrolinx Logfile major mode")
 
 ;; autoload
-(add-to-list 'auto-mode-alist '("\\(coreserver\\|ls-[0-9]\\{2\\})\\.log\\'" . acrolog-mode))
+(add-to-list 'auto-mode-alist '("\\(coreserver\\|ls-[0-9]\\{2\\}\\)\\.log\\(\\.[0-9]+\\)?\\'" . acrolog-mode))
 
-;; '("^[0-9]\\\{4\\}/[0-9]\\{2\\}/[0-9]\\{2\\}" . font-lock-string-face)
+(defvar failure-log-levels-regexp 
+  "\\[\\(WARN \\|ERROR\\|FATAL\\|CRITICAL\\)\\].*$"
+  "Regexp for failure log messages")
+
+;;;; syntax-highlighting
+
+;; error conditions
+(defconst acrolog-font-lock-errors
+  (list
+   '("\\[\\(WARN \\|ERROR\\|FATAL\\|CRITICAL\\)\\].*$" . font-lock-warning-face)
+   '("\\bat [.a-zA-Z():_0-9 \\$]+$" . font-lock-warning-face)
+   '("\\b[\\.a-zA-Z]+Exception" . font-lock-warning-face)
+   '("TERM trapped.*$" . font-lock-warning-face))
+  "Error condition expressions")
+
 ;; syntax highlighting of log levels
 (defconst acrolog-font-lock-log-levels
   (list
@@ -19,12 +33,25 @@
    '("\\(Language Server\\)? ls-[0-9]\\{2\\}-[a-f0-9]+" . font-lock-keyword-face)
    '("^[0-9]\\{4\\}/[0-9]\\{2\\}/[0-9]\\{2\\} [0-9]\\{2\\}:[0-9]\\{2\\}:[0-9]\\{2\\}.[0-9]\\{3\\} |" . font-lock-preprocessor-face)
    '("Available memory.*$" . font-lock-doc-face)
+   '("\\(-->\\|<--\\) Wrapper St.*$" . font-lock-doc-face)
    '("\\[\\(DEBUG\\|INFO \\)\\]" . font-lock-builtin-face)
-   '("\\[\\(WARN \\|ERROR\\|FATAL\\|CRITICAL\\)\\]" . font-lock-warning-face)
-   '("\\bat [.a-zA-Z():_0-9\\$]+$" . font-lock-warning-face)
-   '("\\b[\\.a-zA-Z]+Exception" . font-lock-warning-face)
    )
   "Minimal highlighting expressions for Acrolinx Logfile mode")
+
+(defconst acrolog-font-locks
+  (append acrolog-font-lock-errors
+	  acrolog-font-lock-log-levels)
+  "Default highlighting expression for Acrolog mode")
+
+;; functions
+
+;; next error
+(defun acrolog-next-failure ()
+  "Jump to next WARN, ERROR, CRITICAL, FATAL log message"
+  (interactive)
+  (message "Next failure log message")
+  (re-search-forward "\\[\\(WARN \\|ERROR\\|FATAL\\|CRITICAL\\)\\].*$")
+  (point))
 
 ;; 'main' function
 (defun acrolog-mode ()
@@ -33,7 +60,7 @@
   (kill-all-local-variables)
   (use-local-map acrolog-mode-map)
   
-  (set (make-local-variable 'font-lock-defaults) '(acrolog-font-lock-log-levels))
+  (set (make-local-variable 'font-lock-defaults) '(acrolog-font-locks))
   
   (setq major-mode 'acrolog-mode)
   (setq mode-name "Acrolog")
